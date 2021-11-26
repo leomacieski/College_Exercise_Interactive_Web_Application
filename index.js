6e1721ee4da5f07e646583403ee1b6086d5fc897
leomacieski <leojkmacieski@Hotmail.com>
2021-11-26 11:27:33 +0000

    index working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const   express = required('express'),
+const   express = require('express'),
         fs = require('fs'),
         http = require('http'),
         path = require('path'),
@@ -10,3 +10,29 @@ const   router = express();
         server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'views')));
+
+router.get('/', function(req, res ){
+
+    res.writeHead(200, {'content-Type' : 'text/html'});
+
+    let xml = fs.readFileSync('PaddyCafe.xml', 'utf8'),
+        xsl = fs.readFileSync('PaddyCafe.xsl', 'utf8');
+
+    console.log(xml);
+    console.log(xsl);
+
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xml);
+
+    let result = xsltProcess(doc, stylesheet);
+
+    console.log(result);
+
+    res.end(result.toString());
+
+})
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+    const addr = server.address();
+    console.log('Server listening at ', addr.address + ':' + addr.port)
+})
